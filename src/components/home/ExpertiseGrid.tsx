75b4cecc8559c867cb333012bc6b74e3ef7e2b23
Steven Thierry Ngouala <68360622+Steven23-Ngouala@users.noreply.github.com>
2026-09-03 09:10:24 +0100

    fix: restore Link import in ExpertiseGrid for modal actions and build successfully

--- src/components/home/ExpertiseGrid.tsx
+++ src/components/home/ExpertiseGrid.tsx
@@ -14,6 +14,7 @@ import {
   Clock,
   MessageSquare
 } from 'lucide-react';
+import { Link } from 'react-router-dom';
 import { EXPERTISES_DATA } from '../../data/expertisesData';
 import { COMPANY_INFO } from '../../data/companyInfo';
 import { SubService } from '../../types';
